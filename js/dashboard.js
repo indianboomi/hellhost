eb970dbcbefa1d88f5269aa6db1c2aa00cd47b89
indianboomi <indianboomi157@gmail.com>
2026-06-26 14:35:46 +0530

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,13 +1,33 @@
-if(localStorage.getItem("loggedin")!="true"){
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+
+getAuth,
+
+onAuthStateChanged,
+
+signOut
+
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+// same firebaseConfig here
+
+const app=initializeApp(firebaseConfig);
+
+const auth=getAuth(app);
+
+onAuthStateChanged(auth,user=>{
+
+if(!user){
 
 window.location="login.html";
 
 }
 
-function logout(){
+});
 
-localStorage.removeItem("loggedin");
+window.logout=()=>{
 
-window.location="login.html";
+signOut(auth);
 
 }
